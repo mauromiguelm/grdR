bba040e37be4b2c78c63a4a3b7aa3c4d78c16beb
Mauro Miguel Masiero <mauromiguelm@hotmail.com>
2022-09-30 16:25:41 +0200

    added package sample data

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' grdR input sample data
+#'
+#' Synthetic growth curve in the input format of the grdR package.
+#'
+#'
+#' @format ## `grd`
+#' A data frame with 1,085 rows and 6 columns:
+#' \describe{
+#'   \item{agent}{Country name}
+#'   \item{half eff}{parameter half eff}
+#'   \item{k}{parameter k}
+#'   \item{t_onset}{time of drug onset}
+#'   \item{concentration}{perturbation concentration}
+#'   \item{time}{time point}
+#'   \item{cell_count}{cell count}
+
+#'
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"sample_data"
